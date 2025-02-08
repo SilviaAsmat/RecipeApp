6b8d932bbece5c0ebf5e74e94ed501eb4123b482
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2025-02-07 23:25:53 -0600

    Created "About" page and path

--- recipes/views.py
+++ recipes/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("<h1>Welcome to the Recipes App</h1>")
+
+def about(request):
+    return HttpResponse("<h1>This is a recipes app to track recipes</h1>")
